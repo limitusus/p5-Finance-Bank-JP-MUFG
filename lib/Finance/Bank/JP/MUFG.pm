c4e583356a288851a4449f3dc055ec53101ce282
perforb <dev.perfumed.garden@gmail.com>
2012-06-26 15:45:43 +0900

    initial commit

---
+++ lib/Finance/Bank/JP/MUFG.pm
@@ -0,0 +1,32 @@
+package Finance::Bank::JP::MUFG;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Finance::Bank::JP::MUFG -
+
+=head1 SYNOPSIS
+
+  use Finance::Bank::JP::MUFG;
+
+=head1 DESCRIPTION
+
+Finance::Bank::JP::MUFG is
+
+=head1 AUTHOR
+
+perforb E<lt>dev.perfumed.garden@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
